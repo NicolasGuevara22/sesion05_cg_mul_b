82a34e736825571b42150fd0a127e54a5c96dd0e
Nicolas.Guevara <est.nicolas.guevara@unimilitar.edu.co>
2021-08-26 15:50:04 -0500

    Signed-off-by: Nicolas.Guevara <est.nicolas.guevara@unimilitar.edu.co>

---
+++ Trasnformaciones/src/trasnformaciones/Trasnformaciones.java
@@ -0,0 +1,69 @@
+package trasnformaciones;
+
+import java.util.Scanner;
+
+public class Trasnformaciones {
+
+   
+    public static void main(String[] args) {
+        
+        int x,y;
+		// Defina y asigne dos objetos, un Cadrado y una Circunferencia
+		// Solicite al usuario ingresar el valor de un vector de traslación 
+		// Aplique el vector de traslación anterior a cada uno de los objetos referidos
+		// Muestre las posiciones originales y finales de cada uno de los objetos referidos
+		 System.out.println ("Por favor introduzca los valores del vector de Traslacion: ");
+		 System.out.println ("x: ");
+		 Scanner entrada = new Scanner(System.in);
+		 x = entrada.nextInt();
+		 System.out.println ("y: ");
+		 y = entrada.nextInt();
+	}
+}
+
+class Coord {
+    private int x, y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
+
+class Cuadrado {
+    private Coord c;
+    private int lado;
+    public Cuadrado(Coord c, int lado) {
+        this.c = c;
+        this.lado = lado;
+    }
+    public void Traslacion(Coord tra) {  // Complete los parámetros requeridos
+        c.setX(tra.getX()+ c.getX());
+        c.setY(tra.getY()+ c.getY());
+    }
+    public void Escalado(int sc) {  // Complete los parámetros requeridos
+        lado = (int) (lado *(1+ sc/100.0f));
+    }
+    public void Rotacion() {  // Complete los parámetros requeridos
+        Trasnformaciones rotacion = new Trasnformaciones();
+        rotacion.rotX(Math.toRadians(0));
+    }
+}
+
+class Circunferencia {
+    Coord c;
+    float radio;
+    // Implementar los métodos Traslacion, Escalado y Rotacion para ésta clase
+}
+
